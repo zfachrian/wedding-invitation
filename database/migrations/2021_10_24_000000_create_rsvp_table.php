eb12f7d3cbf52f2cfd7bbd21cbc8a2e753741636
zfachrian <zuhadachmadfachrian@gmail.com>
2021-10-27 19:49:27 +0700

    modify rsvp migration

--- database/migrations/2021_10_24_000000_create_rsvp_table.php
+++ database/migrations/2021_10_24_000000_create_rsvp_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatersvpTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rsvp', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->int('amount')->nullable();
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rsvp');
     }
 }
